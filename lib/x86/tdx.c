e4e2371e746f568b746203d2127d5baf50dca82f
Zhenzhong Duan <zhenzhong.duan@intel.com>
2022-03-03 15:18:51 +0800

    x86 tdx: Introduce basic TDCALL infrastructure
    Enable TDCALL infrastracture by porting tdxcall.S and
    tdcall.S from Linux kernel source code.
    
    lib/x86/tdxcall.S:
      - macro TDX_MODULE_CALL for TDCALL and SEAMCALL
        instructions.
    
      - 3 TDCALL helper functions:
        - __tdcall: Do TDCALL.
        - __tdcall_ret: Do TDCALL and bring back TDCALL return
          values.
        - __tdcall_saved_ret: Do  TDCALL with addtional
          context switch and bring back TDCALL return values,
          most for request service to VMM.
    
    lib/x86/tdx.c:
      - helper __tdx_hypercall() for TDX hypercall.
    
    Signed-off-by: Zhenzhong Duan <zhenzhong.duan@intel.com>
    Reviewed-by: Yu Zhang <yu.c.zhang@intel.com>
    Co-developed-by: Qian Wen <qian.wen@intel.com>
    Signed-off-by: Qian Wen <qian.wen@intel.com>
    Signed-off-by: Yao Yuan <yuan.yao@intel.com>
    
    ---
    v3 -> v2:
        1. Remove _tdx_hypercall() due to it has only 1 caller,
           use __tdx_hypercall() instead. [yuanyao]
        2. Remove #VE related code,they will be introduced with
           #VE handler together. [yuanyao]
        3. Refactor change log. [yuanyao]

--- lib/x86/tdx.c
+++ lib/x86/tdx.c
@@ -19,6 +19,33 @@
 #define TDX_CPUID_LEAF_ID	0x21
 #define TDX_IDENT		"IntelTDX    "
 
+u64 __tdx_hypercall(struct tdx_module_args *args)
+{
+	/*
+	 * For TDVMCALL explicitly set RCX to the bitmap of shared registers.
+	 * The caller isn't expected to set @args->rcx anyway.
+	 */
+	args->rcx = TDVMCALL_EXPOSE_REGS_MASK;
+
+	/*
+	 * Failure of __tdcall_saved_ret() indicates a failure of the TDVMCALL
+	 * mechanism itself and that something has gone horribly wrong with
+	 * the TDX module, so panic.
+	 */
+	if (__tdcall_saved_ret(TDG_VP_VMCALL, args))
+		abort();
+
+	if (args->r10)
+		printf("__tdx_hypercall err:\n"
+		       "R10=0x%016lx, R11=0x%016lx, R12=0x%016lx\n"
+		       "R13=0x%016lx, R14=0x%016lx, R15=0x%016lx\n",
+		       args->r10, args->r11, args->r12, args->r13, args->r14,
+		       args->r15);
+
+	/* TDVMCALL leaf return code is in R10 */
+	return args->r10;
+}
+
 bool is_tdx_guest(void)
 {
 	static int tdx_guest = -1;
