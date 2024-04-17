64621bf8e52d7699bf11d4cd55643f0fa738a454
Yao Yuan <yuan.yao@intel.com>
2024-04-17 09:58:37 +0800

    x86 tdx: Introduce tdx #DB single step emulation test case
    Single step #DB is emulated in mainly 2 scenarios:
    
    1. #VE handler in lib/x86/tdx.c. Some instruction execution
    is converted into #VE for TD guest, i.e. CPUID with specific
    leaf number or WRMSR with specific msr index.[1]
    
    2. Instruction execution VMEXIT handled by TDX without
    return to VMM. i.e. CPUID with leaf 0.
    
    Add test case to validate #DB single step behavior for
    scenarios above.
    
    Signed-off-by: Zhenzhong Duan <zhenzhong.duan@intel.com>
    Reviewed-by: Yu Zhang <yu.c.zhang@intel.com>
    Signed-off-by: Dan Wu <dan1.wu@intel.com>
    Signed-off-by: Qian Wen <qian.wen@intel.com>
    Signed-off-by: Yao Yuan <yuan.yao@intel.com>
    
    [1] https://cdrdv2.intel.com/v1/dl/getContent/795381
    File "cpuid_virtulization.json" and
    File "msr_virtualization.json"
    
    ---
    v2 -> v3:
    1. Separate to single patch. [yuanyao]
    2. Refactor change log & code implementation. [yuanyao]

--- x86/intel_tdx.c
+++ x86/intel_tdx.c
@@ -21,6 +21,90 @@ static void test_selfipi_msr(void)
 	printf("End APIC_SELF_IPI MSR write test.\n");
 }
 
+static volatile unsigned long db_addr[20], dr6[20];
+static volatile unsigned int n;
+static void handle_db(struct ex_regs *regs)
+{
+	db_addr[n] = regs->rip;
+	dr6[n] = read_dr6();
+
+	if (dr6[n] & 0x1)
+		regs->rflags |= (1 << 16);
+
+	if (++n >= 10) {
+		regs->rflags &= ~(1 << 8);
+		write_dr7(0x00000400);
+	}
+}
+
+static void test_single_step(void)
+{
+	extern char ss_0;
+	extern char ss_1;
+	extern char ss_2;
+	extern char ss_3;
+	extern char ss_4;
+	extern char ss_5;
+	extern char ss_6;
+	extern char ss_7;
+	extern char ss_8;
+	extern char ss_9;
+
+	printf("\nStart single step test.\n");
+	handle_exception(DB_VECTOR, handle_db);
+
+	/*
+	 * Test #DB single step emulation for 2 scenarios:
+	 * 1. #VE handler by lib/tdx.c:
+	 *	Trigger by using cpuid(0xb) and wrmsr(0x828)
+	 * 2. instruction vmexit handled by tdx module:
+	 *	Trigger by using cpuid(0)
+	 */
+	n = 0;
+	write_dr6(0);
+	asm volatile(
+			"pushf\n\t"
+			"pop %%rax\n\t"
+			"or $(1<<8),%%rax\n\t"
+			"push %%rax\n\t"
+			"popf\n\t"
+			"and $~(1<<8),%%rax\n\t"
+		".global ss_0; ss_0:\n\t"
+			"push %%rax\n\t"
+		".global ss_1; ss_1:\n\t"
+			"mov $0xb,%%rax\n\t"
+		".global ss_2; ss_2:\n\t"
+			"cpuid\n\t"
+		".global ss_3; ss_3:\n\t"
+			"mov $0x0,%%rax\n\t"
+		".global ss_4; ss_4:\n\t"
+			"cpuid\n\t"
+		".global ss_5; ss_5:\n\t"
+			"movl $0x828,%%ecx\n\t"
+		".global ss_6; ss_6:\n\t"
+			"rdmsr\n\t"
+		".global ss_7; ss_7:\n\t"
+			"wrmsr\n\t"
+		".global ss_8; ss_8:\n\t"
+			"popf\n\t"
+		".global ss_9; ss_9:\n\t"
+		: : : "rax", "ebx", "ecx", "edx");
+
+	report(n == 10 &&
+	       db_addr[0] == (unsigned long)&ss_0 && dr6[0] == 0xffff4ff0 &&
+	       db_addr[1] == (unsigned long)&ss_1 && dr6[1] == 0xffff4ff0 &&
+	       db_addr[2] == (unsigned long)&ss_2 && dr6[2] == 0xffff4ff0 &&
+	       db_addr[3] == (unsigned long)&ss_3 && dr6[3] == 0xffff4ff0 &&
+	       db_addr[4] == (unsigned long)&ss_4 && dr6[4] == 0xffff4ff0 &&
+	       db_addr[5] == (unsigned long)&ss_5 && dr6[5] == 0xffff4ff0 &&
+	       db_addr[6] == (unsigned long)&ss_6 && dr6[6] == 0xffff4ff0 &&
+	       db_addr[7] == (unsigned long)&ss_7 && dr6[7] == 0xffff4ff0 &&
+	       db_addr[8] == (unsigned long)&ss_8 && dr6[8] == 0xffff4ff0 &&
+	       db_addr[9] == (unsigned long)&ss_9 && dr6[9] == 0xffff4ff0,
+	       "single step emulated instructions");
+	printf("End single step test.\n");
+}
+
 int main(void)
 {
 	if (!is_tdx_guest()) {
@@ -29,5 +113,6 @@ int main(void)
 	}
 
 	test_selfipi_msr();
+	test_single_step();
 	return report_summary();
 }
